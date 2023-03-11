e89ec5ce61c434c312f1dbabcc9a9d296dbfba4d
Simon Frankau <sgf@arbitrary.name>
2023-03-11 16:58:22 +0000

    Add first cut at tooling to extract bitmaps from the binary

--- tools/README.md
+++ tools/README.md
@@ -5,3 +5,5 @@
    code in an emulator to get the uncompressed memory image, but I
    decided to decompress it externally. This is my implementation of
    the unpacker.
+ * `extract_images` - Start of tooling to extract bitmapped images
+   from the binary.
---
+++ tools/extract_images/src/main.rs
@@ -0,0 +1,155 @@
+//
+// Image extractor
+//
+// Extract bit-planed Amiga images.
+//
+
+use std::fs;
+use std::path::Path;
+
+use image::RgbImage;
+
+// Game seems to use 5-bit images.
+const DEPTH: usize = 5;
+
+// 32 colour palette
+const PALETTE_SIZE: usize = 1 << DEPTH;
+
+const PALETTE_ADDRS: [(usize, &str); 2] = [(0x0000f2, "palette_game"), (0x000132, "palette_mgmt")];
+
+// Memory locations don't match file locations, since image is loaded
+// at 0x84.
+const IMAGE_BASE: usize = 0x84;
+
+const OUT_DIR: &str = "out";
+
+////////////////////////////////////////////////////////////////////////
+// Cheap wrapper around the image we're producing.
+//
+
+struct Image {
+    width: usize,
+    height: usize,
+    data: Vec<u8>,
+    palette: Vec<(u8, u8, u8)>,
+}
+
+impl Image {
+    fn new(width: usize, height: usize, palette: Vec<(u8, u8, u8)>) -> Image {
+        Image {
+            width,
+            height,
+            data: vec![192; width * height * 3],
+            palette,
+        }
+    }
+
+    fn set_pixel(&mut self, x: usize, y: usize, value: u8) {
+        let idx = (y * self.width + x) * 3;
+        let (r, g, b) = self.palette[value as usize];
+        self.data[idx] = r;
+        self.data[idx + 1] = g;
+        self.data[idx + 2] = b;
+    }
+
+    fn save(&self, path: &Path) -> anyhow::Result<()> {
+        let img =
+            RgbImage::from_vec(self.width as u32, self.height as u32, self.data.clone()).unwrap();
+        img.save(path)?;
+        Ok(())
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+// Draw a bit-plane image.
+//
+
+fn draw_image(
+    data: &[u8],
+    w: usize,
+    h: usize,
+    depth: usize,
+    target: &mut Image,
+    x_off: usize,
+    y_off: usize,
+) {
+    fn bit_at(data: &[u8], bit_addr: usize) -> bool {
+        data[bit_addr >> 3] & (1 << (7 - (bit_addr & 7))) != 0
+    }
+
+    let plane_size = w * h;
+
+    fn slice_at(data: &[u8], depth: usize, plane_size: usize, bit_addr: usize) -> u8 {
+        let mut colour = 0;
+        for i in 0..depth {
+            let bit = bit_at(data, bit_addr + i * plane_size);
+            colour >>= 1;
+            if bit {
+                colour += 16;
+            }
+        }
+        colour
+    }
+
+    for x in 0..w {
+        for y in 0..h {
+            let bit_addr = y * w + x;
+            let colour = slice_at(data, depth, plane_size, bit_addr);
+            target.set_pixel(x + x_off, y + y_off, colour);
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+// Entry point
+//
+
+fn extract_colour(data: &[u8]) -> (u8, u8, u8) {
+    // 3-bit RGB values.
+    let r = data[0] & 0xf;
+    let g = data[1] >> 4;
+    let b = data[1] & 0xf;
+
+    (r * 0x11, g * 0x11, b * 0x11)
+}
+
+// Build a palette from a piece of memory.
+fn build_palette(data: &[u8]) -> Vec<(u8, u8, u8)> {
+    data.chunks(2)
+        .take(PALETTE_SIZE)
+        .map(|v| extract_colour(v))
+        .collect()
+}
+
+
+fn extract_set(data: &[u8], offset: usize, w: usize, h: usize, count: usize, palette: &[u8]) -> Image {
+    let mut img = Image::new(w, h * count, build_palette(palette));
+    let data = &data[offset..];
+    for (idx, block) in data.chunks_exact(w * h * DEPTH / 8).enumerate().take(count) {
+        draw_image(block, w, h, DEPTH, &mut img, 0, idx * h);
+    }
+
+    img
+}
+
+/* Images (locations in file, not RAM):
+   0x17346-0x1af46: 96 x 64 x 5 - 4 monitor images
+ */
+
+fn main() -> anyhow::Result<()> {
+    let data = fs::read("../unpack/out/unpacked.bin")?;
+
+    fs::create_dir_all(OUT_DIR)?;
+
+    let img_data = &data;
+    for (palette_addr, palette_name) in PALETTE_ADDRS.iter() {
+        println!("Running for palette {}", palette_name);
+        let palette = &data[*palette_addr - IMAGE_BASE..];
+        let img = extract_set(img_data, 0x17346, 96, 64, 10, palette);
+        img.save(Path::new(
+            format!("{}/extract-{}.png", OUT_DIR, palette_name).as_str(),
+        ))?;
+    }
+
+    Ok(())
+}
